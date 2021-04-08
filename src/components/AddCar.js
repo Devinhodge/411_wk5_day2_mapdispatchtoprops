67467355257bcc72a414b504961f7c24da3d4539
Devinhodge <d.hodge8891@yahoo.com>
2021-04-07 20:23:14 -0500

    finished

--- src/components/AddCar.js
+++ src/components/AddCar.js
@@ -25,14 +25,14 @@ class AddCar extends Component {
     }
 
     handleSubmit = (e) => {
-        e.preventDefault()
-        const payload = { ...this.state }
-        payload.id = this.props.carTotal + 1
-        delete payload.open
-        console.log("THE CAR", payload)
-        // add this.props.addCar function here
-        // also add this.setState to close the dialog
-    }
+        e.preventDefault();
+        const payload = { ...this.state };
+        payload.id = this.props.carTotal + 1;
+        delete payload.open;
+        console.log("THE CAR", payload);
+        this.props.addCar(payload);
+        this.setState({ open: false });
+    };
 
     componentDidUpdate = (prevProps, prevState) => {
         if (prevState.open !== this.state.open) {
